8520f6d2c10ae6b851d4a7f206a2a4aba0ab759e
eddieoz <osoriojr@gmail.com>
2023-10-07 16:45:18 +0300

    update runtime

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:5000 --log-level=debug
+web: gunicorn --log-level=debug wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,4 @@ from flask_socketio import SocketIO
 # Create a SocketIO instance and run the Flask app if this file is executed directly
 if __name__ == "__main__":
         socketio = SocketIO(app)
+        socketio.run(app, debug=True)
